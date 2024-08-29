136b4d78fea6882cd2973e682b4505dbf1e01127
Alan <alanlebrument@gmail.com>
2024-08-29 10:34:28 +0200

    Initial Commit

---
+++ PlacesApp/PlacesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesAppApp.swift
+//  PlacesApp
+//
+//  Created by Alan Lebrument on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PlacesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
