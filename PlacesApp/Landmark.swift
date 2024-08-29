83981941f0c4a13e1b8565076b9382267c980f14
alandotlbrt <alanlebrument@icloud.com>
2024-08-29 11:28:13 +0200

    Adding JsonFile

---
+++ PlacesApp/Landmark.swift
@@ -0,0 +1,8 @@
+//
+//  Landmark.swift
+//  PlacesApp
+//
+//  Created by Alan Lebrument on 29/08/2024.
+//
+
+import Foundation
---
+++ PlacesApp/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  PlacesApp
+//
+//  Created by Alan Lebrument on 29/08/2024.
+//
+
+import Foundation
