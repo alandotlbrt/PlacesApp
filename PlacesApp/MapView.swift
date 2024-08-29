d05cd61dc38856b97b04fb967771b8c0113d053b
alandotlbrt <alanlebrument@icloud.com>
2024-08-29 11:20:54 +0200

    First look

---
+++ PlacesApp/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  PlacesApp
+//
+//  Created by Alan Lebrument on 29/08/2024.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
