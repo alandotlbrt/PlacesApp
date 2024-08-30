daa8a57b982caca26ddf209cb1cbb50ea7208e2c
alandotlbrt <alanlebrument@icloud.com>
2024-08-30 10:49:52 +0200

    final app

---
+++ PlacesApp/Views/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  PlacesApp
+//
+//  Created by Alan Lebrument on 30/08/2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
